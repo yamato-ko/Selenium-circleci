85766376f624fa8cb7d9190c01880eb82421751d
yamato-ko <117713557+yamato-ko@users.noreply.github.com>
2022-11-14 11:07:45 +0900

    add test_google.py & requirements.txt

---
+++ test_google.py
@@ -0,0 +1,33 @@
+import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+class TestGoogle:
+
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+
+    def setup_class(cls):
+      cls.driver = webdriver.Chrome(ChromeDriverManager().install())
+      cls.driver.maximize_window()
+
+    def setup_method(self):
+      self.driver.get("https://google.com")
+
+    def test_case(self):
+      driver = self.driver
+
+      driver.save_screenshot("test-reports/result_001.png")
+
+      driver.find_element(By.CSS_SELECTOR,"#tsf > div:nth-child(2) > div > div.RNNXgb > div > div.a4bIc > input").send_keys("hoge")
+
+      driver.save_screenshot("test-reports/result_002.png")
+
+      driver.find_element(By.CSS_SELECTOR,"#tsf > div:nth-child(2) > div > div.FPdoLc.VlcLAe > center > input.gNO89b").click()
+
+      driver.save_screenshot("test-reports/result_003.png")
+
+      assert driver.title.count('hoge'), "ページタイトルにhogeが含まれていること"
+
+    def teardown_class(cls):
+        cls.driver.quit()
